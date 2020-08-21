c807af0ddffc365e3ab5f0f08a6860e54be06bc9
Lane Coleman <lane.coleman@live.com>
2020-08-21 08:38:15 -0600

    Add project files.

---
+++ Variables.vb
@@ -0,0 +1,34 @@
+﻿Option Strict On
+Option Explicit On
+Module Variables
+
+    Sub Main()
+        Dim myFirstNumber As Integer
+        Dim mySecondNumber As Integer
+        Dim aLittleString As String
+        Dim bigTestEnergy As Boolean
+        mySecondNumber = 69
+        myFirstNumber = 420
+        aLittleString = "Bruh"
+        bigTestEnergy = True
+        Console.Write(myFirstNumber)
+        Console.ReadLine()
+        Console.Write(mySecondNumber)
+        Console.ReadLine()
+        Console.Write(aLittleString)
+        Console.ReadLine()
+        Console.Write(bigTestEnergy)
+        Console.ReadLine()
+
+        ''break it with wrong data type
+        'mySecondNumber = "5"
+
+        'x = 7
+        'Console.WriteLine(x)
+        'Console.ReadLine()
+
+
+
+    End Sub
+
+End Module
